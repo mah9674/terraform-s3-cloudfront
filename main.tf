8500f5357534fab9624b3900e3de63b175cb6898
Mahasweta Bhattacharjee <bhattacharyamahasweta09@gmail.com>
2026-02-13 18:50:31 +0530

    Added S3 upload step

--- main.tf
+++ main.tf
@@ -1,27 +1,60 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "demo-terraform-bucket-1234567"
+resource "aws_s3_bucket" "website" {
+  bucket = "mahasweta-terraform-site-987654"
 }
-resource "aws_cloudfront_origin_access_control" "oac" {
-  name                              = "demo-oac"
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document {
+    suffix = "index.html"
+  }
 }
-resource "aws_cloudfront_distribution" "cf" {
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.website.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
+  })
+}
+
+# CloudFront Distribution
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  default_root_object = "index.html"
 
   origin {
-    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
     origin_id   = "s3-origin"
-    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+
+    s3_origin_config {
+      origin_access_identity = ""
+    }
   }
 
-  enabled = true
-
   default_cache_behavior {
-    target_origin_id = "s3-origin"
+    target_origin_id       = "s3-origin"
     viewer_protocol_policy = "redirect-to-https"
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
+
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods  = ["GET", "HEAD"]
 
     forwarded_values {
       query_string = false
@@ -31,13 +64,14 @@ resource "aws_cloudfront_distribution" "cf" {
     }
   }
 
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
   restrictions {
     geo_restriction {
       restriction_type = "none"
     }
   }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
+
